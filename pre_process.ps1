25ddee0dbba5a7a2f553d7b3beb8bf5e7315b233
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-09-22 16:56:19 +0530

    Before jenkins

---
+++ pre_process.ps1
@@ -0,0 +1,85 @@
+param ($giturl,$test)
+# argument for test case to run [TODO]
+
+git clone $giturl testRepo
+
+$repo = $(Resolve-FullPath -Path testRepo).Path
+
+$env:Path += ";C:\Program Files\Java\jre1.8.0_301"
+
+$external_variables = Get-Content -raw -Path variables.txt | ConvertFrom-StringData
+$projectName = (Get-Item $repo).Name
+$SDK_LOCATION = $external_variables.'SDK_LOCATION' -replace '[\\]','\$&'
+$NDK_LOCATION = $external_variables.'NDK_LOCATION' -replace '[\\]','\$&'
+# [Regex]::Escape($external_variables.'NDK_LOCATION')
+function ConvertTo-StringData {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+        [HashTable[]]$HashTable
+    )
+    process {
+        foreach ($item in $HashTable) {
+            foreach ($entry in $item.GetEnumerator()) {
+                "{0}={1}" -f $entry.Key, $entry.Value
+            }
+        }
+    }
+}
+function updateLocalProperties{
+    if(!(Test-Path $external_variables.'SDK_LOCATION') -or  !(Test-Path ($external_variables.'SDK_LOCATION'+"\ndk"))){
+        Write-Host "Please update the SDK and NDK location with a valid path"
+    }
+    $LocalProps.'sdk.dir' = $SDK_LOCATION
+    $LocalProps.'ndk.dir' = $NDK_LOCATION
+    ConvertTo-StringData $LocalProps | Set-Content $local_properties.FullName
+}
+# Set-StrictMode -Version Latest
+# $ErrorActionPreference = "Stop"
+# $PSDefaultParameterValues['*:ErrorAction']='Stop'
+$data = @('codeql')
+$data.ForEach({
+    if ($null -eq (Get-Command $PSItem -ErrorAction SilentlyContinue)) 
+    { 
+       Write-Host "Unable to find $PSItem in your PATH"
+       exit 1
+    }
+})
+$local_properties = Get-ChildItem $repo | Where-Object { ($_.PSIsContainer -ne $true) -and ($_.Name -eq "local.properties")}  
+# | Select-Object FullName
+if($null -eq $local_properties){
+    Write-Host "Rewriting local.properties"
+    Set-Content "$repo\\local.properties" "sdk.dir=$SDK_LOCATION"
+    Add-Content "$repo\\local.properties" "ndk.dir=$NDK_LOCATION"
+    # updateLocalProperties
+}
+else{
+    $LocalProps = convertfrom-stringdata (get-content $local_properties.FullName -Raw)
+    if(!(Test-Path $LocalProps.'sdk.dir') -or  !(Test-Path $LocalProps.'ndk.dir')){
+        updateLocalProperties
+    }
+}
+
+Push-Location $repo
+# Start Scan
+codeql database create $projectName --language=java 
+# --overwrite
+# Start Analysis
+codeql database analyze --format=sarif-latest --output=output.json --search-path=C:\Users\elbon\Documents\GitHub\Devaa++\vscode-codeql-starter $projectName "C:\Users\elbon\Documents\GitHub\Devaa++\vscode-codeql-starter\ql\java\ql\test\query-tests\security\CWE-2203\$($test).ql"
+# --rerun
+# Fetch JSON and [parse grammar]
+$jsonObj = Get-Content -raw -Path output.json | ConvertFrom-Json
+$jsonObj.runs.ForEach({
+    $_.results.ForEach({
+        $_.locations.ForEach({ 
+            "$($_.physicalLocation.artifactLocation.uri):$($_.physicalLocation.region.startLine):$($_.physicalLocation.region.startColumn):$($_.physicalLocation.region.endColumn)"
+        })
+    })
+})
+# activity class - bqrs file -> json - get source and sinks -> running test cases
+#  eg. "com.irccloud.android", "com.irccloud.android.activity.SAMLAuthActivity"
+#  run on https://github.com/shivasurya/nextcloud-android.git
+
+# C:\Users\elbon\Documents\GitHub\devaa\examples
+
+Pop-Location
